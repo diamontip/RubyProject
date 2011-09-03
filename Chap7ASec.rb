b6ae4d503fa915c30c4a64db2d7b7140e0022162
Shanmuga Raja <geekraja@gmail.com>
2011-09-03 18:33:06 +0530

    renamed

---
+++ Chap7ASec.rb
@@ -0,0 +1,31 @@
+#collect array values
+puts "Enter text, to end press enter alone"
+freetext = []
+while (input = gets.chomp) !=""
+	freetext << input
+end
+nooftext = freetext.length
+#puts nooftext
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+puts 'no of text: '
+puts nooftext
+
+while nooftext >= 1
+	nooftext1=nooftext-1
+	puts nooftext1
+	if nooftext1 > 0
+		puts 'end here?'
+	else
+end		
+	nooftext -= 1
+	
+end
+
+
+
+if ('string'[/^str/])
+	puts 'yahoo'
+	else
+		puts 'booo'
+	end
+	
